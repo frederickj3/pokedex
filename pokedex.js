a26d3b17f80ac20d568e881cfe35cc39775db76a
Josh Frederick <1joshfrederick@gmail.com>
2024-05-28 11:14:35 -0400

    [Update] pokedex.js
    Added a for loop to retrieve the entire list of pokemon

--- pokedex.js
+++ pokedex.js
@@ -2,7 +2,19 @@ const pokemonCount = 151;
 var pokedex = {}; // {1 : {"name" : bulbasaur, "img" : url, "type" : ["grass", "poison"], "desc" : "...." }}
 
 window.onload = async function(){
-    getPokemon(1);
+    //getPokemon(1);
+
+    for (let i = 1; i <= pokemonCount; i++){
+        await getPokemon(i);
+
+        let pokemon = document.createElement("div");
+        pokemon.id = i;
+        pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+        pokemon.classList.add("pokemon-name");
+        document.getElementById("pokemon-list").append(pokemon);
+    }
+
+    console.log(pokedex);
 }
 
 async function getPokemon(num){
@@ -10,5 +22,15 @@ async function getPokemon(num){
 
     let res = await fetch(url);
     let pokemon = await res.json();
-    console.log(pokemon);
+   
+    let pokemonName = pokemon["name"];
+    let pokemonType = pokemon["types"];
+    let pokemonImg = pokemon["sprites"]["front_default"];
+
+    res = await fetch(pokemon["species"]["url"]);
+    let pokemonDesc = await res.json();
+
+    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"]
+
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types": pokemonType, "desc" : pokemonDesc}
 }
