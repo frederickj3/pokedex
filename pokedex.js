03d281304db65317fbe985e93cc063914405ff44
Josh Frederick <1joshfrederick@gmail.com>
2024-05-28 11:03:28 -0400

    [Update] pokedex.js
    -Added async function to retrieve from the API

---
+++ pokedex.js
@@ -0,0 +1,14 @@
+const pokemonCount = 151;
+var pokedex = {}; // {1 : {"name" : bulbasaur, "img" : url, "type" : ["grass", "poison"], "desc" : "...." }}
+
+window.onload = async function(){
+    getPokemon(1);
+}
+
+async function getPokemon(num){
+    let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+
+    let res = await fetch(url);
+    let pokemon = await res.json();
+    console.log(pokemon);
+}
